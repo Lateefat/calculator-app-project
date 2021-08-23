5b37ab73b770e1a0dc22b87be301c85a0929bf3a
Lateefat <LateefatOba@gmail.com>
2021-08-23 11:22:45 +0100

    fixed some bugs

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let output = document.getElementById("screen");
 let eachkey = Array.from(document.getElementsByClassName("button"));
 let button = document.querySelectorAll("button");
 let equalcounter = 0;
-
-
+// keep track of compounding operations
+let ans = 0;
 
 
 eachkey.map( button => {
@@ -14,6 +14,8 @@ eachkey.map( button => {
       case "RESET":
         output.value = "";
         output.style.fontSize = "32px";
+        equalcounter = 0;
+        ans = 0;
         break;
       case "DEL":
         output.value = output.value.slice(0, -1);
@@ -25,9 +27,24 @@ eachkey.map( button => {
           break;
 
       // display result
-      case "=":
-      
-      console.log(equalcounter)
+      case "=": 
+      // Compounding result   
+      if((ans > 0) && (ans < 10) && (output.value.includes("*")) ||
+      (output.value.includes("/")) ||
+      (output.value.includes("+")) ||
+      (output.value.includes("-"))  
+      )
+      try{
+        output.value = eval(output.value).toLocaleString();
+        equalcounter++;
+        console.log("includes");
+        ans++;
+        console.log(ans); 
+        return;
+      }
+      catch{
+        output.value = "Invalid input";
+      }
       // clear screen after displaying result
       if(equalcounter > 0){
         output.value = "";
@@ -36,8 +53,12 @@ eachkey.map( button => {
       } else
        try{
         output.value = eval(output.value).toLocaleString();
-        equalcounter++;  
+        equalcounter++;
+         
       } catch{
+        if(ans >= 10){
+          output.value = "Overflow";
+        } else
         output.value = "Invalid input";
         }   
         break;
@@ -47,7 +68,7 @@ eachkey.map( button => {
           if(output.value.length > 16){
             output.style.fontSize = "28px";
           }
-          
+             
     }
   });
 });
